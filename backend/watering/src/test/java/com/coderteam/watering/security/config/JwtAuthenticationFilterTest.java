683187590bc33f245b3712b77322d556a3ee1d86
Dang Anh Van <anhvan1999@github.com>
2020-05-01 11:15:23 +0700

    Remove unused import in JwtAuthenticationFilterTest.java

--- backend/watering/src/test/java/com/coderteam/watering/security/config/JwtAuthenticationFilterTest.java
+++ backend/watering/src/test/java/com/coderteam/watering/security/config/JwtAuthenticationFilterTest.java
@@ -6,7 +6,6 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
