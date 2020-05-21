f4dc7061aefe6218deb8bb4db5d3254e470c1deb
Anh Van <danganhvan1999@gmail.com>
2020-05-21 11:19:07 +0700

    Update

--- backend/watering/src/main/java/com/coderteam/watering/info/controller/WebsocketController.java
+++ backend/watering/src/main/java/com/coderteam/watering/info/controller/WebsocketController.java
@@ -2,7 +2,6 @@ package com.coderteam.watering.info.controller;
 
 import org.eclipse.paho.client.mqttv3.IMqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
