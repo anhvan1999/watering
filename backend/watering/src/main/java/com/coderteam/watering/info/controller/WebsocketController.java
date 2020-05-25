73d8261ce5c5a7d4386209087c8e751c7d01a141
Anh Van <danganhvan1999@gmail.com>
2020-05-25 10:35:51 +0700

    Add spring mqtt integration

--- backend/watering/src/main/java/com/coderteam/watering/info/controller/WebsocketController.java
+++ backend/watering/src/main/java/com/coderteam/watering/info/controller/WebsocketController.java
@@ -1,6 +1,5 @@
 package com.coderteam.watering.info.controller;
 
-import org.eclipse.paho.client.mqttv3.IMqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,13 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/ws/websocket")
 public class WebsocketController {
-
-    private IMqttClient client;
-
-    public WebsocketController(IMqttClient client) {
-        this.client = client;
-    }
-
+    
     @GetMapping("")
     public String getWebsocketTestPage() {
         return "websocket";
@@ -25,7 +18,6 @@ public class WebsocketController {
     @GetMapping("/publish")
     @ResponseBody
     public String publish() throws MqttException {
-        client.publish("T_2", "Hello".getBytes(), 1, false);
         return "Publish";
     }
 
---
+++ backend/watering/src/main/java/com/coderteam/watering/mqtt/config/MqttIntegrationConfig.java
@@ -0,0 +1,62 @@
+package com.coderteam.watering.mqtt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.PublishSubscribeChannel;
+import org.springframework.integration.core.MessageProducer;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.MessagingException;
+
+@Configuration
+public class MqttIntegrationConfig {
+    
+    @Bean
+    public MessageChannel mqttInputChannel() {
+        return new PublishSubscribeChannel();
+    }
+
+    @Bean
+    public MessageProducer inbound(MqttProperties mqttProperties) {
+        MqttPahoMessageDrivenChannelAdapter adapter = new MqttPahoMessageDrivenChannelAdapter(
+                mqttProperties.getBrokerUrl(),
+                mqttProperties.getClientId(),
+                mqttProperties.getTopicFilter()
+        );
+        adapter.setConverter(new DefaultPahoMessageConverter());
+        adapter.setQos(1);
+        adapter.setOutputChannel(mqttInputChannel());
+        return adapter;
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "mqttInputChannel")
+    public MessageHandler handler() {
+        return new MessageHandler() {
+
+            @Override
+            public void handleMessage(Message<?> message) throws MessagingException {
+                System.out.println(message.getPayload());
+            }
+
+        };
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "mqttInputChannel")
+    public MessageHandler handler2() {
+        return new MessageHandler() {
+
+            @Override
+            public void handleMessage(Message<?> message) throws MessagingException {
+                System.out.println("------");
+            }
+
+        };
+    }
+
+}
