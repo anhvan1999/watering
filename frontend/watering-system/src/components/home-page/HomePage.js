f7932965ae54e220537889d76c5bbf0a31281ecb
Dang Anh Van <anhvan1999@github.com>
2020-06-04 16:25:14 +0700

    Fix bug in loginpage

--- frontend/watering-system/src/components/home-page/HomePage.js
+++ frontend/watering-system/src/components/home-page/HomePage.js
@@ -1,57 +1,44 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 import style from './homepage.module.scss';
 import { getClassName } from '../../utils/component-utils';
-import LoginPage from '../login-page/LoginPage';
-
 
 export default function HomePage() {
-    
-    return (
-        <Router>
-            <div className={getClassName(
-                style.Bgimg, style.HomePageContainer, style.AnimeOpicity, style.TextWhite
-            )}>
-                <div className={getClassName(style.BannerDisplayMiddle)}>
-                    <h1 className={
-                        getClassName(style.HomePageSkeleton,
-                            style.WelcomeWeight,
-                            style.WelcomeJumbo,
-                            style.WelcomeAnimateTop,
-                            style.ContentCenter)
-                    }>
-                        Welcome to <br /> Watering System
+  return (
+    <div className={getClassName(
+      style.Bgimg, style.HomePageContainer, style.AnimeOpicity, style.TextWhite
+    )}>
+      <div className={getClassName(style.BannerDisplayMiddle)}>
+        <h1 className={
+          getClassName(style.HomePageSkeleton,
+            style.WelcomeWeight,
+            style.WelcomeJumbo,
+            style.WelcomeAnimateTop,
+            style.ContentCenter)
+        }>
+          Welcome to <br /> Watering System
                 </h1>
-                    <hr className={
-                        getClassName(style.BorderColor,
-                            style.BorderSize,
-                            style.BorderAnimation)
-                    } />
-                    <p className={
-                        getClassName(
-                            style.ContentLarge,
-                            style.ContentCenter,
-                            style.ContentFormat)}>
-                        The Website will assist you in irrigating your plants easily with our features.
+        <hr className={
+          getClassName(style.BorderColor,
+            style.BorderSize,
+            style.BorderAnimation)
+        } />
+        <p className={
+          getClassName(
+            style.ContentLarge,
+            style.ContentCenter,
+            style.ContentFormat)}>
+          The Website will assist you in irrigating your plants easily with our features.
                     <br /> Let's sign in and try to use these features.
                 </p>
 
-                    <Link to="/login" className={getClassName(
-                        style.ButtonFontSize,
-                        style.ButtonFormat,
-                        style.ButtonCenter)}>
-                        Sign In
-                    </Link>
+        <Link to="/login" className={getClassName(
+          style.ButtonFontSize,
+          style.ButtonFormat,
+          style.ButtonCenter)}>
+          Sign In</Link>
 
-
-
-                </div>
-            </div>
-            <Switch>
-                <Route exact path="/login">
-                    <LoginPage />
-                </Route>
-            </Switch>
-        </Router>
-    );
+      </div>
+    </div>
+  );
 }
