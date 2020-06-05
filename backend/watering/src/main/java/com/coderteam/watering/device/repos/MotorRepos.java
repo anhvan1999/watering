541d433ffc65a2b70d6c9b388a8c81eb2cefb1b9
Dang Anh Van <anhvan1999@github.com>
2020-06-05 11:09:39 +0700

    add @Repostory annotation on MotoRepos

--- backend/watering/src/main/java/com/coderteam/watering/device/repos/MotorRepos.java
+++ backend/watering/src/main/java/com/coderteam/watering/device/repos/MotorRepos.java
@@ -2,9 +2,11 @@ package com.coderteam.watering.device.repos;
 
 import com.coderteam.watering.device.entity.Motor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface MotorRepos extends JpaRepository<Motor, Long> {
 
     Optional<Motor> findByDeviceId(String deviceId);
--- backend/watering/src/main/java/com/coderteam/watering/device/repos/MotorStatusRepos.java
+++ backend/watering/src/main/java/com/coderteam/watering/device/repos/MotorStatusRepos.java
@@ -2,7 +2,9 @@ package com.coderteam.watering.device.repos;
 
 import com.coderteam.watering.device.entity.MotorStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MotorStatusRepos extends JpaRepository<MotorStatus, Long> {
 
 }
