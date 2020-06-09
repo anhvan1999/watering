5625d899575ad75b93815363849d1dc153473f16
nhantran1245 <ssaiyan123410@gmail.com>
2020-06-10 00:53:16 +0700

    fix sensorInfo css

---
+++ frontend/watering-system/src/components/SensorInfo/sensorInfo-row.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './sensorInfo.scss';
+
+export default function SensorInfoRow(props) {
+    return (
+        <tr>
+            <td>Cảm biến {props.id}</td>
+            <td>{props.measure}</td>                     
+            <td>{props.state}</td>                     
+            <td><button className="btn btn-primary btn-sensor" onClick = {() => props.func(props.id)}></button></td>
+        </tr>
+
+    )
+}
